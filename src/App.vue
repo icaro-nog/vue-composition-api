2bd6007e32b587bfabf1094feebecdd92665d12f
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2025-07-25 15:25:16 -0300

    Introdução a composition API Props e Emit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+  import { RouterLink, RouterView } from 'vue-router'
+  import HelloWorld from './components/HelloWorld.vue'
+  import AppButton from './components/AppButton.vue';  
+
+  const getUpdate = (data) => {
+    console.log('getUpdate', data)
+  }
 </script>
 
 <template>
@@ -8,13 +13,23 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <!-- <HelloWorld msg="You did it!" /> -->
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/reatividade">Reatividade</RouterLink>
       </nav>
+
+      Hello vue <br>
+
+      <AppButton
+        data-vue="Jon"
+        @update="getUpdate"
+      >
+        Save
+        <template #icon>Icon</template>
+      </AppButton>
     </div>
   </header>
 
---
+++ src/components/AppButton.vue
@@ -0,0 +1,32 @@
+<script setup>
+    import { defineProps, useAttrs, defineEmits } from 'vue';
+
+    const props = defineProps({
+        variant: {
+            type: String,
+            default: 'success'
+        }
+    })
+
+    const attrs = useAttrs()
+    const emit = defineEmits()
+
+    console.log(props.variant)
+    console.log(attrs)
+
+    function sendData(){
+        emit('update', props.variant)
+    }
+
+</script>
+
+<template>
+    <button @click="sendData">
+        {{ variant}}
+        <slot></slot>
+        <slot name="icon"></slot>
+    </button>
+</template>
+
+<style>
+</style>
